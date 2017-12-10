0325d0a31117bb97d7123bfd3b5b0e4bfc22a6cf
Stan004 <flywww004@gmail.com>
2017-12-11 01:15:54 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,4 @@
 class Admin::RestaurantsController < ApplicationController
-  before_action :authenticate_user!
   before_action :authenticate_admin
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
@@ -46,7 +45,7 @@ def set_restaurant
 end
 
 def restaurant_params
-  params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
+  params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image, :category_id)
 end
 
 end
