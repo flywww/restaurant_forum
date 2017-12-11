aad97cd03a72417decd167acd57b5fd50983cc1d
Stan004 <flywww004@gmail.com>
2017-12-12 00:26:56 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :restaurants, only: [:index, :show]
   root "restaurants#index"
 
   namespace :admin do
     resources :restaurants
-    resources :categories 
+    resources :categories
     root "restaurants#index"
   end
 
