50bea44829b8b6a3e436e20c7832e6fdcf3f7f3b
Stan004 <flywww004@gmail.com>
2017-12-23 01:23:19 +0800

    add show and edit erb for users and finish avatar uploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path
+      flash[:notice] = "user was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "user was failed to update"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :intro, :avatar)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  mount_uploader :avatar, AvatarUploader
          def admin?
            self.role == "admin"
          end
