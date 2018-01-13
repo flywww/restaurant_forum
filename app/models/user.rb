e5617c9b632699b28ac59b4dc6295131ed6d3ee6
Stan004 <flywww004@gmail.com>
2018-01-14 00:41:13 +0800

    add favorite model and setup relation among user, retaurant and favorit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
+  # 「使用者評論很多餐廳」的多對多關聯
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
+  # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
